6c46dfffda899dbe4f6bbe00ad40d9e51586f0f4
Taylan Pince <taylanpince@gmail.com>
2023-12-06 22:43:58 +0100

    Initial commit with basic command line support

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+const { Command } = require("commander");
+const figlet = require("figlet");
+const program = new Command();
+console.log(figlet.textSync("TPS Report"));
+program
+    .version("1.0.0")
+    .description("Run TPS reports against Sequence Relayer")
+    .option("-c, --chain  [value]", "Chain to run report on")
+    .parse(process.argv);
+const options = program.opts();
+if (options.chain) {
+    console.log("Selected chain: ", options.chain);
+}
+//# sourceMappingURL=index.js.map
